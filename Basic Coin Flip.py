37230c238f45448f4e5d0df87ae96273864ed0d7
hudsonthomas55 <hudsonthomas55@gmail.com>
2023-03-28 20:10:09 -0400

    switch to a GUI program rather than CLI only

--- Basic Coin Flip.py
+++ Basic Coin Flip.py
@@ -1,20 +1,38 @@
-# Import module
+# Imports
 import random
+from tkinter import *
 
-# Define coin, two sides to coin
-coin = [1,2]
+# Window setup
+window = Tk()
+window.minsize(width=500, height=250)
+window.title("Coin Flip")
+window.config(padx=50, pady=50)
 
-# Define coin flip process
-coin_flip = input("Please press 'Enter' to flip the coin. ")
-coin_outcome = random.choice(coin)
+# Create program
+def run_program():
+    """Runs the coin flip program"""
+
+    # Define coin, two sides to coin
+    coin = [1,2]
+
+    # Define coin flip process
+    coin_outcome = random.choice(coin)
+
     # Assign heads
-if coin_outcome == 1:
-    print("Heads!")
+    if coin_outcome == 1:
+        print("Heads!")
 
     # Assign tails
-elif coin_outcome == 2:
-    print("Tails!")
+    elif coin_outcome == 2:
+        print("Tails!")
 
     # Allow for troubleshooting
-else:
-    print("error")
+    else:
+        print("error")
+
+# Create button to run program
+coin_button = Button(text="Flip Coin", width=50, command=run_program)
+coin_button.pack()
+
+# Run window
+window.mainloop()
